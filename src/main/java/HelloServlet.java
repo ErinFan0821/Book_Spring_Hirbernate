4885e009d600d3ccf4b6ef2a5071bf3e90030325
techops <fanxiao1987821@gmail.com>
2014-04-24 21:22:58 +0800

    Post success!

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -4,32 +4,29 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 /**
  * Created by techops on 4/24/14.
  */
 public class HelloServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String url = "jdbc:mysql://localhost:3306/BookShelf" ;
-        String username = "root" ;
-        String password = "root" ;
-        try{
-            Connection con =
-                    DriverManager.getConnection(url, username, password);
+        String isbn = request.getParameter("isbn");
+        String name = request.getParameter("name");
+        String price = request.getParameter("price");
+        String author = request.getParameter("author");
 
-        }catch(SQLException se){
-            System.out.println("数据库连接失败！");
-            se.printStackTrace() ;
-        }
+        PrintWriter writer = response.getWriter();
+        writer.println(isbn);
+        writer.println(name);
+        writer.println(price);
+        writer.println(author);
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter writer = response.getWriter();
-        writer.println("<h1>" + "BookShelf,get" + "</h1>");
-        writer.println("<form action=\"add\">"+
+        writer.println("<h1>" + "BookShelf" + "</h1>");
+        writer.println("<form action=\"Hello\" method=\"post\">"+
                 "ISBN: <input type=\"text\" name=\"isbn\" />\n" +
                 "Name: <input type=\"text\" name=\"name\" />\n" +
                 "Price: <input type=\"text\" name=\"price\" />\n"+
